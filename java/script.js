10dae4bcec76d995a8e8b473e678fff417960ecf
Austin Coil <austinjeffree@gmail.com>
2020-03-10 22:31:22 -0500

    put tabs in for menu page

--- java/script.js
+++ java/script.js
@@ -1,38 +1,13 @@
-function openMenuList(evt, menuList) {
-    var i, tabContent, tabLinks;
-    tabContent = document.getElementsByClassName("tabContent");
-    for (i=0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
+function openMenu(evt, menuName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
-    tabLinks = getElementsByClassName("tabLinks");
-    for (i=0; i < tabLinks.length; i++) {
-        tabLinks[i].className = tabLinks[i].className.replace("active", "")
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(menuList).style.display = "block";
-    evt.currentTarget.className += "active";
-}
-
-var slideInterval = 3500
-
-function getImages() {
-    return document.getElementsbyid('musicImgs').getElementsByTagName('figure');
-}
-function slideShow() {
-    var pointer;
-    var images = getImages();
-    for (var i = 0; i < images.length; i++ ) {
-        if (images[i].className == 'visible') {
-            images[i].className = "";
-            pointer = i
-    }
-        if (++pointer == images.length) {
-            pointer = 0;
-        }
-        figures[pointer].className = 'visible';
-        setTimeout(slideShow, slideInterval;)
-    }
-}
-function startSlideshow() {    
-        setTimeout(slideShow, slideInterval;)
-}
-startSlideshow();
+    document.getElementById(menuName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
